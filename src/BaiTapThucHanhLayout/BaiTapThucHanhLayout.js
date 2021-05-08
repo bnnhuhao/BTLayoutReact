0277761c36072ab7af3994821611607414ff5fee
Hao <bnnhuhao@gmail.com>
2021-05-08 14:51:42 +0700

    update file

--- src/BaiTapThucHanhLayout/BaiTapThucHanhLayout.js
+++ src/BaiTapThucHanhLayout/BaiTapThucHanhLayout.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
-import Carousel from './Carousel'
-import Footer from './Footer'
-import Header from './Header'
-import ListCard from './ListCard'
+import BT3Content from './BT3Content'
+import BT3Footer from './BT3Footer'
+import BT3Header from './BT3Header'
 
 export default class BaiTapThucHanhLayout extends Component {
     render() {
         return (
             <div>
-                <Header/>
-                <Carousel/>
-                <ListCard/>
-                <Footer/>
+                <BT3Header/>
+                <BT3Content/>
+                <BT3Footer/>
             </div>
         )
     }
